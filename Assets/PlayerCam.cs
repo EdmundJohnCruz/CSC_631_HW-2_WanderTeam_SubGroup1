87d1297367c597e6ae9a1f4d373de251ad55ec41
EdmundJohnCruz <edmundjohncruz@gmail.com>
2021-02-13 22:09:26 -0800

    Camera Switching by Pressing Q
    Inputted a player camera that switches between behind the player and a WIP top down camera by pressing Q. Also added a delay timer for pressing Q.

---
+++ Assets/PlayerCam.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCam : MonoBehaviour
+{
+    public Transform player; //gets coord information of player into a variable
+    public Vector3 offset; //offset for the back camera on the player
+    public Vector3 topDown;
+    public bool switchPers = false;
+    public float timeBetwQ = 0.500f; //allow half a second before pressing Q again
+    public float timestamp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("q") && Time.time >= timestamp)
+        {
+            timestamp = Time.time + timeBetwQ;
+            switchPers = !switchPers; //switches between true and false
+            Debug.Log(switchPers); //seeing what value is given in console
+        }
+
+        if(switchPers == false)
+        {
+            transform.position = topDown;
+        }
+
+        if(switchPers == true)
+        {
+            transform.position = player.position + offset; //behind the player view
+        }
+    }
+}
