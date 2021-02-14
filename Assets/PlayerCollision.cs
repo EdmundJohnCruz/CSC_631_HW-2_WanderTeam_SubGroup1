ef6caacb82e2f1e6abcf190bb85048c4fc0b9afa
EdmundJohnCruz <edmundjohncruz@gmail.com>
2021-02-13 23:53:48 -0800

    Collision Effects
    Player's movement will be disabled and the cube will turn white once collided with the object. This was made using a player collision script.

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement; //creates a variable for the movement script to be utilized within this variable
+    
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("Obstacle Hit.");
+            movement.enabled = false; //movement gets disabled
+            transform.GetComponent<Renderer>().material.color = Color.white; //player turns white when colliding with the obstacle
+        }
+    }
+}
